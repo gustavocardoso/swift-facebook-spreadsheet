14fb4c228798c41c9ac3f3d5689e55851899c855
Gustavo Cardoso <gustavocardoso@gmail.com>
2021-03-08 10:02:13 -0800

    Lower the number of results

--- src/modules/getFacebookData.js
+++ src/modules/getFacebookData.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const getFacebookData = async query => {
   const response = await axios.get(
-    `https://graph.facebook.com/v9.0/act_502570590155794/insights?access_token=${process.env.FACEBOOK_API_ACCESS_TOKEN}&date_preset=lifetime&level=ad&fields=account_name,campaign_name,adset_name,ad_name,campaign_id,adset_id,ad_id,reach,impressions,spend,inline_link_clicks,inline_link_click_ctr,cost_per_inline_link_click,actions,cost_per_action_type&limit=999&filtering=[{field: "action_type",operator:"IN", value: ["lead"]}]&sort=['adset_id_descending']`
+    `https://graph.facebook.com/v9.0/act_502570590155794/insights?access_token=${process.env.FACEBOOK_API_ACCESS_TOKEN}&date_preset=lifetime&level=ad&fields=account_name,campaign_name,adset_name,ad_name,campaign_id,adset_id,ad_id,reach,impressions,spend,inline_link_clicks,inline_link_click_ctr,cost_per_inline_link_click,actions,cost_per_action_type&limit=900&filtering=[{field: "action_type",operator:"IN", value: ["lead"]}]&sort=['adset_id_descending']`
   )
 
   const { data } = await response.data
